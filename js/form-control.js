18a778aa268258e43a6a7d84b18bb19988b97d10
rtavaresramos <rtavareskxd99@gmail.com>
2020-06-13 22:07:23 -0300

    adding proc page to ORA Website

--- js/form-control.js
+++ js/form-control.js
@@ -1,5 +1,25 @@
 
 
 function handleForm(){
-  alert('formulario enviado')
+  setTimeout(function(){ 
+  while (   
+    document
+    .getElementById('proc-form')
+    .firstChild) {
+    document
+    .getElementById('proc-form').
+    removeChild(
+    document
+    .getElementById('proc-form')
+    .firstChild);
+  } 
+
+  document
+  .getElementById('proc-form')
+  .innerHTML = `<div class="proc-sent"><h1>Formulário enviado!</h1>
+  <p>Em até 48 horas, entraremos em contato por e-mail para seguirmos com a sua solicitação judicial.</p>
+  </div>`
+  
+}, 7000);
+
 }
