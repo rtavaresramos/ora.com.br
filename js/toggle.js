25dab9bb72b42ae3f259aacec2958da1b4bff685
rtavaresramos <rtavareskxd99@gmail.com>
2020-06-20 00:27:55 -0300

    update about the directories from the new organization to all site

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,9 @@
 var e = document.getElementById("toggle-control")
 
 function handleToggle(){
-e.classList.toggle("toggle-off")
-}
+  e.classList.toggle("toggle-off")
+  e.classList.toggle("toggle-on")
+  if(e.classList.contains("toggle-off")){
+    e.classList.add("desactive")
+  }
+  }
