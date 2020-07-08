1ded7b345de93ab3cea11612c6bf80ca0ef367d8
rtavaresramos <rtavareskxd99@gmail.com>
2020-07-07 21:56:21 -0300

    adding a new format for toggle button

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,10 @@
 var e = document.getElementById("toggle-control")
+var toggleActive = document.getElementById("toggle-hamburger-input")
 
-function handleToggle(){
+toggleActive.addEventListener('change',()=>{
   e.classList.toggle("toggle-off")
   e.classList.toggle("toggle-on")
   if(e.classList.contains("toggle-off")){
     e.classList.add("desactive")
   }
-  }
+  })
