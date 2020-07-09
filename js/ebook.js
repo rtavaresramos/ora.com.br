4454760b0dd3124b7f6dafb8264a7174c0a2c34f
rtavaresramos <rtavareskxd99@gmail.com>
2020-07-09 15:54:53 -0300

    adding the ebook link

--- js/ebook.js
+++ js/ebook.js
@@ -1,8 +1,11 @@
 var button = document.getElementById('cta-ebook')
+var buttonA = document.getElementById('cta-ebook-a')
 
 
 function onloadEbook(){
 setTimeout(() => {
+  button.classList.toggle('display-off')
   button.classList.toggle('display-on')
+  
 }, 15000);
 }
