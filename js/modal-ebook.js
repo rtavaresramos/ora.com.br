3966038cc9c26cdd23da01a2dbe5703e8d603843
Rodrigo Tavares <rtavareskxd99@gmail.com>
2020-08-25 11:40:48 +0000

    getting corrections for modal

--- js/modal-ebook.js
+++ js/modal-ebook.js
@@ -1,15 +1,23 @@
 
-var count = 0
+var count = localStorage.getItem('hadSeenModal')
 
 document.getElementById('close').addEventListener('click', ()=>{
     document.getElementById('modal').style.display = 'none'
 })
 
 window.addEventListener('mouseout', ()=>{
-    if(count ===0){
+    if(count === 0 || count === null || count === undefined){
         setTimeout(() => {            
-            document.getElementById('modal').style.display = 'block'
+            document.getElementById('modal').style.display = 'flex'
             count++
+            localStorage.setItem('hadSeenModal', count)
         }, 5000);
     }
 });
+
+document.getElementById('cta-ebook').addEventListener('click', ()=>{
+    setTimeout(() => {
+        window.open( " https://drive.google.com/file/d/16wtiftQeiJxa8ljVSiZEcBNsyIJiEjiu/view", "_blank" )
+    }, 500);
+    document.getElementById('modal').style.display = 'none'
+})
